45b2470393ddea2134ad805039385b3bf6973a99
Michael Rollins <michael.rollins@gmail.com>
2023-07-26 13:10:43 -0400

    Initial Commit

---
+++ self-employed-tax-estimator/self_employed_tax_estimatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  self_employed_tax_estimatorApp.swift
+//  self-employed-tax-estimator
+//
+//  Created by Michael Rollins on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct self_employed_tax_estimatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
